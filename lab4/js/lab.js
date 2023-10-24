73d87029fe299b7dfc9df03e4bf0d3bc6832385f
ksuw688 <ksu29@ucsc.edu>
2023-10-24 00:11:57 -0700

    upload

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,15 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+//Rock-Paper-Scissors Game
+//Kye Gorosave <kgorosav@ucsc.edu>
+//Mon Oct. 23 2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//Explain the rules of the game
+  //Paper loses to scissors, rock loses to paper, and scissors lose to rock. 
+//Ask the player for their choice at the beginning of each turn
+  //Choose rock or paper or scissors
+//Generate a random choice for either rock, paper, or scissors.
+  //random function with a choice of either rock, paper, or scissors
+//Compare player vs computer choice
+  //if(choice=same) then display “ITS A DRAW”
+      //else if player chooses rock and computer scissors, OR player chooses scissors and computer chooses paper, OR player chooses paper and computer chooses rock DISPLAY “YOU WIN!”
+  //else display “COMPUTER WINS!”
+//Show computers choice and see if it was a win, draw, or loss and then ask them if they want to play again
