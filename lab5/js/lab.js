dfbd03ab195e2d9f33a9628eaf204bc451c85d07
ksuw688 <ksu29@ucsc.edu>
2023-10-27 03:20:14 -0700

    upload

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Author:    kaiwen Su / Kye Gorosave
+ * Created:   10.27.2023
+ * 
+ * (c) Copyright by Kye Gorosave & Kaiwen Su.
+ **/
 
-// Constants
+// Define Variables
+make = "Dodge";
+model = "Charger";
+color = "Silver”;
+year = 2016;
 
-// Functions
+// Calculate
+age = 2023 - year;
+
+// output
+document.writeln("Make:" + make + "<br>")
+document.writeln("Model:" + model + "<br>")
+document.writeln("Color:" + color + "<br>")
+document.writeln("Year:" + year + "<br>")
+document.writeln("Age:" + age + " years<br>")
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
