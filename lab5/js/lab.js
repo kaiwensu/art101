aa21119686906dffbd65d714aeaeefa16ce15f66
ksuw688 <ksu29@ucsc.edu>
2023-10-27 03:59:29 -0700

    upload

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,36 +1,55 @@
-/**
- * Author:    kaiwen Su / Kye Gorosave
- * Created:   10.27.2023
- * 
- * (c) Copyright by Kye Gorosave & Kaiwen Su.
- **/
+/*
+  * Author: Kye Gorosave & Kevin Su
+  * Created: 27 October 2023
+*/
 
-// Define Variables
-make = "Dodge";
-model = "Charger";
-color = "Silver”;
-year = 2016;
+//Define the variables
+var make;
+var model;
+var color;
+var year;
+var currentYear;
+var age;
+var ownIt = false;
 
-// Calculate
-age = 2023 - year;
+//Declaring the variables
+make = "Audi";
+model = "Sport Quattro S1";
+color = "White";
+year = 1986;
+currentYear = 2023;
+//Age calculation
+age = currentYear - year;
+//Output 
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years old<br>");/*
+* Author: Kye Gorosave & Kevin Su
+* Created: 27 October 2023
+*/
 
-// output
-document.writeln("Make:" + make + "<br>")
-document.writeln("Model:" + model + "<br>")
-document.writeln("Color:" + color + "<br>")
-document.writeln("Year:" + year + "<br>")
-document.writeln("Age:" + age + " years<br>")
+//Define the variables
+var make;
+var model;
+var color;
+var year;
+var currentYear;
+var age;
+var ownIt = false;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//Declaring the variables
+make = "Audi";
+model = "Sport Quattro S1";
+color = "White";
+year = 1986;
+currentYear = 2023;
+//Age calculation
+age = currentYear - year;
+//Output 
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years old<br>");
